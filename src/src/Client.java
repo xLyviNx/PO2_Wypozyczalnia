5fb74449c52873eef02a815bca5c2724a4b8908f
LyviN <LyviNSFM@gmail.com>
2023-12-28 17:39:16 +0100

    jak w trakcie programu serwer sie zamknie to teraz tez rozlaczy klienta

--- src/src/Client.java
+++ src/src/Client.java
@@ -1,6 +1,7 @@
 package src;
 import java.net.*;
 import java.io.*;
+import java.nio.Buffer;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +10,8 @@ public class Client {
     public Socket socket;
     private DataInputStream input;
     private DataOutputStream output;
+    private BufferedInputStream bufInput;
+    private BufferedOutputStream bufOutput;
     public void start(String address, int port) {
         if (instance != null)
             return;
@@ -27,14 +30,31 @@ public class Client {
             socket = new Socket(address, port);
             input = new DataInputStream(socket.getInputStream());
             output = new DataOutputStream(socket.getOutputStream());
-
+            bufInput = new BufferedInputStream(input);
+            bufOutput = new BufferedOutputStream(output);
             Scanner scanner = new Scanner(System.in);
             String receivedString;
 
             while (socket.isConnected())
             {
-
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException iex) {
+                    throw new RuntimeException(iex);
+                }
+                try {
+                    output.writeUTF("ping");
+                    output.flush();
+                }catch (SocketException socketexc)
+                {
+                    System.out.println("ERROR PINGING");
+                    if(WypozyczalniaOkno.instance != null)
+                    {
+                        WypozyczalniaOkno.instance.NoConnection();
+                    }
+                }
             }
+            System.out.println("DISCONNECTED.");
         } catch (ConnectException e) {
             System.out.println("Nie można połączyć się z serwerem: " + e.getMessage());
             if(WypozyczalniaOkno.instance != null)
